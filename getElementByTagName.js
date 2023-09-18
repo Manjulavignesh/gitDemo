cf433bf286227940aeb9445b8cf4820801386951
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-09-18 20:04:17 +0530

    getElementByTagName

---
+++ getElementByTagName.js
@@ -0,0 +1,8 @@
+var elements=document.getElementsByClassName('list-group-item list-group-item-action');
+elements[0].style.color = "black";
+elements[0].style.backgroundColor = "Violet";
+var elements1=document.getElementsByTagName("li");
+for(let i=0;i<elements1.length;i++)
+{
+    elements1[i].style.color="red";
+}
