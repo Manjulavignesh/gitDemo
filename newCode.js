e34c3e722277170434414e30c6780f830e3aeafa
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-09-18 16:37:08 +0530

    JScode

---
+++ newCode.js
@@ -0,0 +1,4 @@
+let header=document.getElementById('main-header');
+header.style.borderBottom="solid 3px #000";
+let elements = document.getElementsByClassName('title');
+  elements[0].style.color='green';
