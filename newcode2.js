241625b7b1fb1f52624b11b0bdc704d02541b5d3
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-09-18 19:23:27 +0530

    newcode3

---
+++ newcode2.js
@@ -0,0 +1,8 @@
+
+var elements = document.getElementsByClassName('list-group-item');
+    elements[2].style.backgroundColor = 'green';
+
+    for(let i=0;i<elements.length;i++)
+    {
+      elements[i].style.font = 'bold 20px';
+    }
